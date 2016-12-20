a0657a4cd188ce8b48ab2e95d91fcbcf1e275c81
Marshal <marshal.wu@qq.com>
2016-12-21 00:55:47 +0800

    add exit code

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const restify = require('restify');
+const random = require("random-js")();
 
 const server = restify.createServer();
 server.get('/hello/:name', (req, res, next) => {
+    let {
+        name
+    } = req.params;
+
+    if (name == 'test') {
+        // 模拟非正常退出
+        if (random.integer(0, 9) > 6) {
+            process.exit(1);
+        }
+    }
+
     res.send({
         status: 'OK'
     });
