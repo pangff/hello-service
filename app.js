92f362607df07424c76531389298f59fc460c73e
“pangff” <admin@example.com>
2017-01-17 21:54:05 +0800

    add log4j

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const restify = require('restify');
 const random = require("random-js")();
+const log4js = require('log4js');
+log4js.configure({
+  appenders: [
+    { type: 'console' },{
+      type: 'file', 
+      filename: 'logs/access.log', 
+      maxLogSize: 1024,
+      backups:4,
+      category: 'normal' 
+    }
+  ],
+  replaceConsole: true
+});
+
+// app.use(app.router);
+const logger = log4js.getLogger('normal');
+logger.setLevel('INFO');
 
 const server = restify.createServer();
 server.get('/hello/:name', (req, res, next) => {
@@ -17,8 +34,9 @@ server.get('/hello/:name', (req, res, next) => {
     res.send({
         status: 'OK'
     });
+    logger.info("getName");
     next();
 });
 server.listen(3000, function() {
-    console.log('%s listening at %s', server.name, server.url);
+    logger.info('%s listening at %s', server.name, server.url);
 });
